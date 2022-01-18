f94a7b4527e74739a05b874724bbb4bd6233a79f
gunelibrahimova <gunel.ibrahimova.2001@gmail.com>
2022-01-18 23:40:22 +0400

    18.01.2022

---
+++ Methods/Go.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_project.Methods
+{
+    public class Go
+    {
+        public void ImtahanBali(decimal bal)
+        {
+            Console.WriteLine("Giris balinizi daxil edin:");
+            string girisBali = Console.ReadLine();
+
+            
+
+            Console.WriteLine("Imtahandan topladiginiz bali daxil edin");
+
+            string imtahanBali = Console.ReadLine();
+
+
+            decimal umumiBal = Convert.ToInt32(girisBali) + Convert.ToInt32(imtahanBali);
+
+
+            decimal eksikBal = 51 - Convert.ToInt32(umumiBal);
+
+            if (Convert.ToInt32(umumiBal) < 51 && Convert.ToInt32(girisBali) > 0)
+            {
+                Console.WriteLine("Siz " + eksikBal + " balla kesildiniz");
+
+            }
+            else 
+            {
+                //Console.WriteLine("Siz kecdiniz");
+                if (51 <= Convert.ToInt32(umumiBal) && Convert.ToInt32(umumiBal) <= 61)
+                {
+                    Console.WriteLine("Siz E aldiniz");
+                }
+                else if (61 <= Convert.ToInt32(umumiBal) && Convert.ToInt32(umumiBal) <= 71)
+                {
+                    Console.WriteLine("Siz D aldiniz");
+                }
+                else if (71 <= Convert.ToInt32(umumiBal) && Convert.ToInt32(umumiBal) <= 81)
+                {
+                    Console.WriteLine("Siz C aldiniz");
+                }
+                else if (81 <= Convert.ToInt32(umumiBal) && Convert.ToInt32(umumiBal) <= 91)
+                {
+                    Console.WriteLine("Siz B aldiniz");
+                }
+                else
+                {
+                    Console.WriteLine("Siz A aldiniz");
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+using Student_project.Methods;
+using Student_project.Models;
+Evvele:
+Console.WriteLine("1.Telebe haqqinda melumat");
+Console.WriteLine("2.Imtahan verdiyi fenn");
+Console.WriteLine("3.Imtahandan kecib yoxsa kesilib");
+
+
+
+string daxilEdilenReqem = Console.ReadLine();
+
+Student student = new();
+
+Go start = new Go();
+
+decimal bal = 100M;
+
+switch (Convert.ToInt32(daxilEdilenReqem))
+{
+    case 1:
+        Console.WriteLine("Adinizi ve soyadinizi daxil edin");
+        student.Name = Console.ReadLine();
+        goto Evvele;
+        break;
+    case 2:
+        Console.WriteLine("Imtahan verdiyiniz fennin adini daxil edin");
+        student.Fenn = Console.ReadLine();
+        goto Evvele;
+        break;
+    case 3:
+        start.ImtahanBali(bal);
+        break;
+
+}
+
+
